f33865f38c6ebfc25bca02f407694eccfae4fd58
Josh Walker <j.walker1005@outlook.com>
2026-03-09 01:06:39 +0000

    fix: Multiply identity by scale vector for correct ScaleMat4

--- maths/src/main/scala/linalg/matrices/ScaleMat.scala
+++ maths/src/main/scala/linalg/matrices/ScaleMat.scala
@@ -7,8 +7,8 @@ import ginseng.maths.linalg.vectors.Vec.*
 type ScaleMat4 = SqrMat[4]
 
 object ScaleMat4 {
-    def apply(s: Vec3): TranslateMat4 = {
+    def apply(s: Vec3): ScaleMat4 = {
         val SqrMat(a, b, c, d) = SqrMat.identity[4]
-        SqrMat(a, b, c, s :+ 1)
+        SqrMat(a * s(0), b * s(1), c * s(2), d)
     }
 }
