82c87812b5bbda79fcf0921a7fc97fa1d4fdbcf6
Josh Walker <j.walker1005@outlook.com>
2026-03-30 00:59:54 +0100

    feat: Replace triShader with generic shader interpolating over n colours

--- renderer/src/main/scala/shaders/Shaders.scala
+++ renderer/src/main/scala/shaders/Shaders.scala
@@ -34,29 +34,33 @@ object Shaders {
         .link()
 
 
-    def triShader(using Zone): ShaderProg = ShaderLoader()
+    def interpolateShader(colours: Colour*)(using Zone): ShaderProg = ShaderLoader()
         // Define vertex shader - set rgb values at vertices
-        .load(Shader.vertex(
-            """#version 410 core
-            |in vec3 vp;
-            |out vec4 vertex_color;
-            |vec4 colors[3] = vec4[3](
-            |    vec4(1, 0, 0, 1),
-            |    vec4(0, 1, 0, 1),
-            |    vec4(0, 0, 1, 1)
-            |);
-            |void main() {
-            |  gl_Position = vec4( vp, 1.0 );
-            |  vertex_color = colors[gl_VertexID % 3];
-            |}""".stripMargin
-        ))
+        .load({
+            val n = colours.length
+            val rgba = colours.map(colour => {
+                    val (r, g, b, a) = colour.toFloatRGB
+                    s"vec4($r, $g, $b, $a)"
+                })
+
+            Shader.vertex(
+                s"""#version 410 core
+                |in vec3 vp;
+                |out vec4 vertex_colour;
+                |vec4 colours[$n] = vec4[$n](${rgba.mkString(", ")});
+                |void main() {
+                |  gl_Position = vec4(vp, 1.0);
+                |  vertex_colour = colours[gl_VertexID % $n];
+                |}""".stripMargin
+            )
+        })
         // Define fragment shader - interpolates vertex colour
         .load(Shader.frag(
             """#version 410 core
-            |in vec4 vertex_color;
-            |out vec4 frag_color;
+            |in vec4 vertex_colour;
+            |out vec4 frag_colour;
             |void main() {
-            |  frag_color = vertex_color;
+            |  frag_colour = vertex_colour;
             |}""".stripMargin
         ))
         .link()
