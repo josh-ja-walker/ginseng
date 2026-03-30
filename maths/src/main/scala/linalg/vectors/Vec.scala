71f99eecf9572aae5da08a62c5b67ad916adb580
Josh Walker <j.walker1005@outlook.com>
2026-03-30 01:30:14 +0100

    feat: implement Vec transpose

--- maths/src/main/scala/linalg/vectors/Vec.scala
+++ maths/src/main/scala/linalg/vectors/Vec.scala
@@ -4,34 +4,37 @@ import scala.compiletime.ops.any.==
 import scala.compiletime.ops.int.*
 import scala.annotation.targetName
 
-import slash.{vector as slash}
+import slash.vector.{Vec as SlashVec}
+import slash.matrix.{Mat as SlashMat}
 
 import ginseng.maths.linalg.matrices.*
 
 
-type Vec[N <: Int] = slash.Vec[N]
+type Vec[N <: Int] = SlashVec[N]
 
 object Vec {
-    def apply[N <: Int](values: Double*)(using ValueOf[N]) = slash.Vec[N](values*)
+    def apply[N <: Int](values: Double*)(using ValueOf[N]) = SlashVec[N](values*)
     def unapplySeq[N <: Int](vec: Vec[N]) = vec.asNativeArray.toSeq
 
     extension[N <: Int] (v: Vec[N]) {
 
+        def transpose(using ValueOf[N]): Mat[1, N] = SlashMat(v.asNativeArray)
+
         def normalized: Vec[N] = v / v.norm
 
-        infix def dot(u: Vec[N]): Double = slash.Vec.dot(v)(u)
+        infix def dot(u: Vec[N]): Double = SlashVec.dot(v)(u)
 
         // Take first M values of vector
         def take[M <: Int](using ValueOf[M], M < N =:= true): Vec[M] =
-            slash.Vec(v.asNativeArray.take(valueOf[M]))
+            SlashVec(v.asNativeArray.take(valueOf[M]))
 
         @targetName("append")
         infix def :+(d: Double)(using ValueOf[+[N, 1]]): Vec[+[N, 1]] = 
-            slash.Vec(v.asNativeArray :+ d)
+            SlashVec(v.asNativeArray :+ d)
 
         @targetName("concat")
         infix def ++[M <: Int](u: Vec[M])(using ValueOf[+[N, M]]): Vec[+[N, M]] = 
-            slash.Vec(v.asNativeArray ++ u.asNativeArray)
+            SlashVec(v.asNativeArray ++ u.asNativeArray)
         
         def toSeq: Seq[Double] = v.asNativeArray.toSeq
         
@@ -40,7 +43,7 @@ object Vec {
     extension[N <: Int] (d: Double) {
         @targetName("prepend")
         infix def +:(v: Vec[N])(using ValueOf[+[1, N]]): Vec[+[1, N]] = 
-            slash.Vec(d +: v.asNativeArray)
+            SlashVec(d +: v.asNativeArray)
     }
 
 }
