aa4becc1f87891439c2d8d6fd65a7f5397d8c50d
Josh Walker <j.walker1005@outlook.com>
2026-04-06 19:59:36 +0100

    feat: darken triangle colours

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -25,6 +25,7 @@ import scala.util.Random
 
 
 @main def main: Unit = Zone {
+
     val config = new ConfigBuilder()
         .withSize(800, 600)
         .withName("Hello Triangle")
@@ -32,14 +33,18 @@ import scala.util.Random
         .build
 
     val context = Context(config)
+    
+    // Define hello triangle
 
     var tri = Triangle.equilateral(2)
     val triShader = Shaders.interpolateShader(
-        Colour.hex("#EACECA"),
-        Colour.hex("#D7E5D3"),
-        Colour.hex("#DBE5F8")
+        Colour.hex("#B85450"),
+        Colour.hex("#82B366"),
+        Colour.hex("#6C8EBF")
     )
     
+    
+    // Define lines forming a grid
 
     def grid(n: Int): Seq[Line] = {
         (1 until n)
@@ -62,10 +67,10 @@ import scala.util.Random
     }
 
 
-    var t = 25 // Animation timestep - start halfway through growth phase
+    // Animation timestep - start halfway through growth phase
+    var t = 25 
 
     context.run(() => {
-
         // Draw grid lines
         lineRenderer.render(lineShader)
         boldLineRenderer.render(boldLineShader)
