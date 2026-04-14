48affd913c2fc671d6467ca3c4c3bcff80b5df1b
Josh Walker <j.walker1005@outlook.com>
2026-04-14 21:57:35 +0100

    fix: normalize direction vectors before dot product when calculating angle between vectors

--- maths/src/main/scala/geometry/Dir.scala
+++ maths/src/main/scala/geometry/Dir.scala
@@ -13,8 +13,8 @@ case class Dir(x: Double, y: Double, z: Double)
    
    
     /** Compute angle between two direction vectors */
-    infix def angle(t: Dir): Angle = Rad(math.acos(this.dot(t)))
-    
+    infix def angle(t: Dir): Angle = Rad(math.acos(this.normalized.dot(t.normalized)))
+
     /** Rotate direction vector anticlockwise about origin */
     infix def rotate(theta: Angle): Dir = {
         val newDir: Vec[3] = RotateMat3(theta, Dir.forward.take[3]) * this.take[3]
