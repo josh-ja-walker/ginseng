48f64cb585e1e748877546d5a78ea6f26a226cf6
Josh Walker <j.walker1005@outlook.com>
2026-04-15 01:33:57 +0100

    fix[Vertex.scala]: override equals to handle floating point precision errors

--- core/src/main/scala/primitives/component/Vertex.scala
+++ core/src/main/scala/primitives/component/Vertex.scala
@@ -6,6 +6,7 @@ import ginseng.maths.linalg.vectors.*
 import ginseng.maths.geometry.vectors.*
 
 import Mat.*
+import Vec.*
 
 import ginseng.core.primitives.*
 import ginseng.core.transformations.*
@@ -17,10 +18,25 @@ case class Vertex[T <: Primitive](index: Int, pos: Pos)(using val host: T)
     def translate(v: Dir): Vertex[T] = Vertex(index, pos + v)
     def reposition(pos: Pos): Vertex[T] = Vertex(index, pos)
 
+    override def equals(obj: Any): Boolean = obj match {
+        case v: Vertex[?] => {
+            v.host == host 
+                && v.index == index 
+                && v.pos.toSeq.zip(pos.toSeq)
+                    .forall((a, b) => (a - b).abs < Vertex.eps)
+        }
+
+        case _ => false
+    }
+
 }
 
 
 object Vertex {
+
+    // TODO: standardise for all equalities
+    // Floating point precision
+    val eps = 0.0001
 
     extension (v: Vertex[Triangle]) {
         def modify(f: Vertex[Triangle] => Vertex[Triangle]): Triangle = {
