74cf4d688178328196d62ff8fe8794126b606d3a
Josh Walker <j.walker1005@outlook.com>
2026-04-15 01:32:45 +0100

    feat: create AngleComponent for modification

---
+++ core/src/main/scala/primitives/component/AngleComponent.scala
@@ -0,0 +1,77 @@
+package ginseng.core.primitives.component
+
+import ginseng.core.primitives.*
+import ginseng.core.transformations.*
+
+import ginseng.maths.angle.*
+import ginseng.maths.linalg.matrices.*
+import ginseng.maths.linalg.vectors.*
+import ginseng.maths.geometry.vectors.*
+
+import Vec.*
+import Dir.*
+
+
+case class AngleComponent[T <: Primitive](ab: Edge[T], bc: Edge[T])
+    extends Component[T] { 
+
+    // Assert that the edges share a vertex along the angle
+    // i.e., edges are contiguous
+    require(ab.b == bc.a)
+
+    // Assert edges share the same host primitive
+    require(ab.host == bc.host)
+    val host: T = ab.host
+
+    val angle: Angle = Dir.angle(-ab.dir)(bc.dir)
+
+    // Helpers for accessing vertices of angle
+    val a: Vertex[T] = ab.a
+    val b: Vertex[T] = ab.b
+    val c: Vertex[T] = bc.b
+
+    def unapply: (Vertex[T], Vertex[T], Vertex[T]) = (a, b, c)
+
+
+    // TODO: implement maths operations on Angle to avoid unnecessary conversion 
+    // Scale angle with respect to bisecting center line
+    def *(factor: Float): AngleComponent[T] = update(Rad(angle.toRadians * factor))
+    def /(factor: Float): AngleComponent[T] = this * (1 / factor)
+
+    // Add/subtract angle in the anticlockwise direction
+    def +(angle: Angle): AngleComponent[T] = rotate(Rad(0), angle)
+    def -(angle: Angle): AngleComponent[T] = this + (Rad(-angle.toRadians))
+
+    // Rotate both edges in the same direction
+    def rotate(angle: Angle): AngleComponent[T] = rotate(Rad(-angle.toRadians), angle)
+
+    // Set angle value
+    def update(angle: Angle): AngleComponent[T] = {
+        val diff: Rad = Rad((angle.toRadians - this.angle.toRadians) / 2)
+        rotate(-diff, diff) // Achieve angles by rotating edges the required diff
+    }
+
+    // Rotate edge AB an additional theta anticlockwise around B
+    // Rotate edge BC an additional phi anticlockwise around B
+    private def rotate(theta: Angle, phi: Angle): AngleComponent[T] = {
+        val perp: Dir = (ab.dir.take[3]).cross(bc.dir.take[3]) :+ 0
+        AngleComponent(ab.rotate(ab.b, theta, perp), bc.rotate(bc.a, phi, perp))
+    }
+
+}
+
+
+object AngleComponent {
+
+    extension (angle: AngleComponent[Triangle]) {
+        def modify(f: AngleComponent[Triangle] => AngleComponent[Triangle]): Triangle = {
+            val verts = angle.host.mat.columnVectors
+            val newAngle = f(angle)
+            verts(angle.a.index) = newAngle.a.pos
+            verts(angle.b.index) = newAngle.b.pos
+            verts(angle.c.index) = newAngle.c.pos
+            new Triangle(Mat[4, 3](verts*))
+        }
+    }
+
+}
