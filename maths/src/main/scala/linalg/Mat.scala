a1ebe13d7efd339d40c8bd5391c3c421eb1b906e
Josh Walker <j.walker1005@outlook.com>
2026-04-17 21:50:22 +0100

    refactor: change Mat constructor parameter to nested Array

--- maths/src/main/scala/linalg/Mat.scala
+++ maths/src/main/scala/linalg/Mat.scala
@@ -4,12 +4,15 @@ import scala.compiletime.ops.any.==
 import scala.compiletime.ops.int.*
 import scala.annotation.targetName
 
-
 export Mat.*
+export SqrMat.*
 
 
-class Mat[R <: Int, C <: Int](val slashMat: slash.matrix.Mat[R, C])
-    (using ValueOf[R], ValueOf[C]) {
+class Mat[R <: Int, C <: Int](val vectors: Array[Array[Double]])(using ValueOf[R], ValueOf[C]) {
+
+    protected private[linalg] val slashMat: slash.matrix.Mat[R, C] = 
+        slash.matrix.Mat[C, R](vectors.flatten).transpose
+
 
     // Index into column vectors
     def apply(i: Int): Vec[R] = cols(i)
@@ -19,14 +22,14 @@ class Mat[R <: Int, C <: Int](val slashMat: slash.matrix.Mat[R, C])
     
     def toSeq: Seq[Vec[R]] = cols
 
-    def transpose: Mat[C, R] = new Mat(slashMat.transpose)
+    def transpose: Mat[C, R] = Mat.fromSlash(slashMat.transpose)
 
 
     // Mathematic operations
     def *(vec: Vec[C]): Vec[C] = Vec.fromSlash(slashMat * vec.slashVec)
 
     def *[N <: Int](mat: Mat[C, N])(using ValueOf[N]): Mat[R, N] = 
-        new Mat(slashMat * mat.slashMat)
+        Mat.fromSlash(slashMat * mat.slashMat)
 
 
     // TODO: implement following methods via Iterable interface 
@@ -34,16 +37,15 @@ class Mat[R <: Int, C <: Int](val slashMat: slash.matrix.Mat[R, C])
     def :+(v: Vec[R])(using ValueOf[C + 1]): Mat[R, C + 1] = Mat.fromSeq(cols :+ v)
 
     infix def ++[N <: Int](n: Mat[R, N])(using ValueOf[N], ValueOf[C + N]): Mat[R, C + N] = 
-        new Mat(slashMat.concatenateColumns[N](n.slashMat))
+        Mat.fromSlash(slashMat.concatenateColumns[N](n.slashMat))
 
     infix def concatRows[N <: Int](n: Mat[N, C])(using ValueOf[N], ValueOf[R + N]): Mat[R + N, C] = 
-        new Mat(slashMat.concatenateRows[N](n.slashMat))
+        Mat.fromSlash(slashMat.concatenateRows[N](n.slashMat))
 
 
     // Take first R vectors
     def take[N <: Int](using ValueOf[N], N < C =:= true): Mat[R, N] =
         Mat.fromSlashVecs(slashMat.columnVectors.take(valueOf[N]))
-
 
 }
 
@@ -62,14 +64,17 @@ object Mat {
     
 
     def identity[M <: Int, N <: Int](using ValueOf[M], ValueOf[N]): Mat[M, N] =
-        new Mat(slash.matrix.Mat.identity[M, N])
+        Mat.fromSlash(slash.matrix.Mat.identity[M, N])
 
+
+    private[linalg] inline def fromSlash[R <: Int, C <: Int](slashMat: slash.matrix.Mat[R, C])
+        (using ValueOf[R], ValueOf[C]): Mat[R, C] = 
+            new Mat[R, C](slashMat.transpose.asNativeArray2D)
 
     // Note: use convention of composing Matrix from column Vectors
     // Requires constructing slash Mat from horizontal vectors and transposing
     private def fromSlashVecs[R <: Int, C <: Int](vecs: Array[slash.vector.Vec[R]])
         (using ValueOf[R], ValueOf[C]): Mat[R, C] = 
-            new Mat(slash.matrix.Mat(vecs).transpose)
-
+            Mat.fromSlash(slash.matrix.Mat(vecs).transpose)
 
 }
