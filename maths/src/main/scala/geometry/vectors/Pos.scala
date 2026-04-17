1e48963f5817cad23b99ee0fa2889d06c4dbbfee
Josh Walker <j.walker1005@outlook.com>
2026-04-17 17:47:33 +0100

    refactor: change Pos to case class inheriting Vec

--- maths/src/main/scala/geometry/vectors/Pos.scala
+++ maths/src/main/scala/geometry/vectors/Pos.scala
@@ -6,47 +6,53 @@ import ginseng.maths.angle.*
 import ginseng.maths.geometry.vectors.*
 import ginseng.maths.geometry.matrices.*
 
-import ginseng.maths.linalg.vectors.*
-import ginseng.maths.linalg.matrices.*
+import ginseng.maths.linalg.*
 
 import Vec.* 
 
 
 /** Position Vector */
-type Pos = Vec4
+
+case class Pos(x: Double, y: Double, z: Double, w: Double = 1.0d) 
+    extends Vec[4](slash.vector.Vec[4](x, y, z, w)) {
+    
+    // Note: homogenous coordinate value must be > 0 for a Position Vector
+    require(w >= 0)
+
+
+    /* Transformations */
+
+    // TODO: check if rotation around origin is equal to no translation
+
+    /** Rotate position by counterclockwise angle in Z axis */
+    infix def rotate(theta: Angle): Pos = this.rotate(theta, Dir.forward)
+
+    /** Rotate position by counterclockwise angle */
+    infix def rotate(theta: Angle, axis: Dir): Pos = Pos(take[3].rotate(theta, axis) :+ w)
+
+    /** Rotate position by counterclockwise angle around a position */
+    infix def rotate(theta: Angle, around: Pos, axis: Dir = Dir.forward): Pos = {
+        // Translate coordinates so around is at origin, then rotate and translate back 
+        val translation = TranslateMat(around.take[3])
+        (translation.inverse * RotateMat4(theta, axis) * translation) * p
+    }
+
+}
+
 
 object Pos {
-    // Note: homogenous coordinate value is 1 for a Position Vector
-    def apply(x: Double, y: Double, z: Double = 0d): Pos = Vec4(x, y, z, 1)
+
+    def apply(v: Vec[4]): Pos = Pos(v.x, v.y, v.z, v.w)
+
 
     // TODO: move window specific positions to Window? 
-    def origin = Pos(-1, -1, 0)
     
     def topLeft = Pos(-1, 1, 0)
     def topRight = Pos(1, 1, 0)
-    def bottomLeft = origin
+    def bottomLeft = Pos(-1, -1, 0)
     def bottomRight = Pos(1, -1, 0)
 
+    def origin = bottomLeft
     def center = Pos(0, 0, 0)
-
-
-    extension (p: Pos) {
-
-        /* Transformations */
-
-        /** Rotate position by counterclockwise angle */
-        infix def rotate(theta: Angle, axis: Dir = Dir.forward): Pos = Vec4.rotate(p)(theta, axis) 
-
-        /** Rotate position by counterclockwise angle around a position */
-        infix def rotateAround(theta: Angle, around: Pos, axis: Dir = Dir.forward): Pos = {
-            // Translate coordinates so around is at origin, then rotate and translate back 
-            val toOrigin = TranslateMat(around.take[3])
-            (-toOrigin * RotateMat4(theta, axis) * toOrigin) * p
-        }
-
-        //FIXME: cannot distinguish between Dir and Pos for overloading of rotote
-        // infix def rotate(theta: Angle, around: Pos): Pos = p.rotate(theta, Dir.forward, around)
-        
-    }
 
 }
