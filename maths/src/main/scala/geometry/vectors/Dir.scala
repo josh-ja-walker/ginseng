eb0b8949acec86bcc642b8f058f168340c524479
Josh Walker <j.walker1005@outlook.com>
2026-04-19 01:29:42 +0100

    refactor: change Dir to case class extending homogenous vector

--- maths/src/main/scala/geometry/vectors/Dir.scala
+++ maths/src/main/scala/geometry/vectors/Dir.scala
@@ -1,16 +1,24 @@
 package ginseng.maths.geometry.vectors
 
 import ginseng.maths.angle.*
-import ginseng.maths.linalg.vectors.* 
+import ginseng.maths.linalg.*
 
 
 /** Direction Vector */
-type Dir = Vec4
+case class Dir(x: Double, y: Double, z: Double)
+    extends Vec[4](Array(x, y, z, 0.0d)) {
+
+    // Note: homogenous coordinate value must be 0 for a Direction Vector
+    require(this.w == 0.0d)
+   
+    /** Compute angle between two direction vectors */
+    infix def angle(t: Dir): Angle = Rad(math.acos(this.dot(t)))
+    
+}
+
 
 object Dir {
-    // Note: homogenous coordinate value is 0 for a Direction Vector
-    def apply(x: Double, y: Double, z: Double = 0d): Dir = Vec4(x, y, z, 0)
-    
+
     val up = Dir(0, 1, 0)
     val down = Dir(0, -1, 0)
     
@@ -22,10 +30,5 @@ object Dir {
 
     val one = Dir(1, 1, 1)
     val zero = Dir(0, 0, 0)
-
-    extension (s: Dir) {
-        /** Compute angle between two direction vectors */
-        infix def angle(t: Dir): Angle = Rad(math.acos(s.dot(t)))
-    }
 
 }
