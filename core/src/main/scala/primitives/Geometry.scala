351858080c76007618be9217bdcfea7d0bacef40
Josh Walker <j.walker1005@outlook.com>
2026-04-30 15:01:59 +0100

    feat: modify Geometry to define center and use extension methods

--- core/src/main/scala/primitives/Geometry.scala
+++ core/src/main/scala/primitives/Geometry.scala
@@ -1,38 +1,63 @@
 package ginseng.core.primitives
 
+
+import ginseng.core.primitives.components.* 
+
 import ginseng.maths.linalg.*
 import ginseng.maths.geometry.*
 
 
 trait Geometry[T <: Poly[?]] {
-    def points(t: T): Array[Pos]
-    def construct(points: Pos*): T // TODO: require(points.length == valueOf[N])
+ 
+    def construct(positions: Pos*): T // TODO: require(points.length == valueOf[N])
+    
+    extension (t: T)
+        def positions: Array[Pos]
+        
+        def center: Pos = {
+            val sum = positions
+                .map(p => p: Vec[4])
+                .reduce((p, q) => p + q) 
+            (sum / positions.length).toPos
+        }
+    
 }
 
 
 given Geometry[Point] with {
-    override def points(t: Point): Array[Pos] = Array(t.pos)
     override def construct(points: Pos*): Point = Point(points(0))
+    
+    extension (t: Point)
+        override def positions: Array[Pos] = Array(t.pos)
+        override def center: Pos = t.pos
 }
 
 given Geometry[Line] with {
-    override def points(t: Line): Array[Pos] = t.mat.toPositions.toArray
     override def construct(points: Pos*): Line = Line(new Mat(points))
+
+    extension (t: Line)
+        override def positions: Array[Pos] = t.mat.toPositions.toArray
 }
 
 given Geometry[Triangle] with {
-    override def points(t: Triangle): Array[Pos] = t.mat.toPositions.toArray
     override def construct(points: Pos*): Triangle = Triangle(new Mat(points))
+
+    extension (t: Triangle)
+        override def positions: Array[Pos] = t.mat.toPositions.toArray
+        override def center: Pos = Line(t.a.pos, Line(t.b.pos, t.c.pos).mid)
+            .intersect(Line(t.b.pos, Line(t.a.pos, t.c.pos).mid))
+            .getOrElse(super.center(t))
 }
 
 given Geometry[Box] with {
-    override def points(t: Box): Array[Pos] = {
-        val Box(a, b, c, d) = t
-        Array(a, b, c, d)
-    }
-
     override def construct(points: Pos*): Box = {
         val Seq(a, b, c, d) = points
         Box(a, b, c, d)
     }
+
+    extension (t: Box) 
+        override def positions: Array[Pos] = {
+            val Box(a, b, c, d) = t
+            Array(a, b, c, d)
+        }
 }
