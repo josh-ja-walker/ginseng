409ea3925e385083b64d7de5eb80ee468a91783c
Josh Walker <j.walker1005@outlook.com>
2026-05-02 17:47:04 +0100

    feat: define vertices and edges for Square

--- core/src/main/scala/poly/polygons/Square.scala
+++ core/src/main/scala/poly/polygons/Square.scala
@@ -1,19 +1,32 @@
 package ginseng.core.poly.polygons
 
+import ginseng.core.poly.geometry.*
+import ginseng.core.poly.geometry.given
 import ginseng.core.poly.components.*
+import ginseng.core.poly.components.given
 
 import ginseng.maths.*
 import ginseng.maths.units.*
 import ginseng.maths.geometry.*
+import ginseng.maths.geometry.given
 
 
 case class Square(a: Pos, b: Pos, c: Pos, d: Pos)
     extends Polygon[4] {
 
-    def ab: Edge[Square] = ???
-    def bc: Edge[Square] = ???
-    def cd: Edge[Square] = ???
-    def da: Edge[Square] = ???
+    type A = 0; type B = 1; type C = 2; type D = 3
+
+    // helpers for referencing edges
+    val ab: Edge[Square] = this.edge[A, B] 
+    val bc: Edge[Square] = this.edge[B, C] 
+    val cd: Edge[Square] = this.edge[C, D] 
+    val da: Edge[Square] = this.edge[D, A] 
+
+    // helpers for referencing angles
+    val alpha: Arc[Square] = this.angle[D, A, B]
+    val beta: Arc[Square] = this.angle[A, B, C]
+    val gamma: Arc[Square] = this.angle[B, C, D]
+    val delta: Arc[Square] = this.angle[C, D, A]
 
 }
 
