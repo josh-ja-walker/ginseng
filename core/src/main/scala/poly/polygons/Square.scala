97f904324dc830f6c0229386657ffc3e8ace7e8b
Josh Walker <j.walker1005@outlook.com>
2026-05-04 14:17:34 +0100

    feat: define Square constructors

--- core/src/main/scala/poly/polygons/Square.scala
+++ core/src/main/scala/poly/polygons/Square.scala
@@ -1,5 +1,6 @@
 package ginseng.core.poly.polygons
 
+import ginseng.core.transformations.given
 import ginseng.core.poly.geometry.*
 import ginseng.core.poly.geometry.given
 import ginseng.core.poly.components.*
@@ -11,18 +12,22 @@ import ginseng.maths.geometry.*
 import ginseng.maths.geometry.given
 
 
-opaque type Square = Quad
+type Square = Quad
+
 
 object Square {
 
     def unapply(s: Square) = Quad.unapply(s)
 
-    def size(length: Length): Square = ???
+    def size(length: Length): Square = Square(Pos.origin, Pos.origin + Dir(1, 1, 0) * length.toDouble) 
     def unital: Square = Square.size(1.u)
 
-    def centered(center: Pos, size: Length): Square = ???
+    def centered(center: Pos, size: Length): Square = 
+        Square.size(size).repositioned(_.center, center)
 
-    def apply(leftBottom: Pos, topRight: Pos): Square = ???
-
+    def apply(bottomLeft: Pos, topRight: Pos): Square = {
+        val Pos(x1, y1, z1, _) = bottomLeft; val Pos(x2, y2, z2, _) = topRight 
+        Quad(bottomLeft, Pos(x2, y1, z2), topRight, Pos(x1, y2, z1))
+    }
 }
 
