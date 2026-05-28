25a7171e1d778d3f35986bab8f85859fd4b6c82b
Josh Walker <j.walker1005@outlook.com>
2026-05-28 18:58:56 +0100

    feat: implement AABB bounds

--- core/src/main/scala/ast/mesh/Bounds.scala
+++ core/src/main/scala/ast/mesh/Bounds.scala
@@ -20,12 +20,90 @@ object Bounds {
 
     // Bounding box aligned upon the axis
     case class AABB(mesh: Mesh[?]) extends Bounds {
-        def resolve(anchorType: AnchorType): Pos = ???
+        def resolve(anchorType: AnchorType): Pos = {
+            
+            def allPositions(mesh: Mesh[?], offset: Dir = Dir.zero): Seq[Pos] = mesh match {
+
+                case AnchorAt(anchor, mesh, at) => {
+                    val anchorPositions = anchor.mesh
+                        .map(allPositions(_, offset))
+                        .getOrElse(Nil) 
+
+                    val meshPositions = allPositions(mesh, (offset + (anchor.pos - at.pos)).toDir)
+
+                    (anchorPositions ++ meshPositions).distinct
+                }
+                    
+                case p: Primitive[?] => p.mat.toPositions.map(_ + offset)
+                
+                case Rendered(mesh, shader) => allPositions(mesh)
+                case Scaffold(mesh) => allPositions(mesh)
+                    
+            }
+
+            PointCloud(allPositions(mesh)).resolve(anchorType)
+        }
     }
+    
 
     // Bounding box orientated in the direction of the bound object
     case class OBB(mesh: Mesh[?]) extends Bounds {
         def resolve(anchorType: AnchorType): Pos = ???
+    }
+
+
+    private class PointCloud(positions: Seq[Pos]) extends Bounds {
+        def resolve(anchorType: AnchorType): Pos = {
+            val minX = positions.minBy(_.x).x
+            val minY = positions.minBy(_.y).y
+            val minZ = positions.minBy(_.z).z
+            
+            val maxX = positions.maxBy(_.x).x
+            val maxY = positions.maxBy(_.y).y
+            val maxZ = positions.maxBy(_.z).z
+            
+            import AnchorType.*
+
+            anchorType match {
+                case A => Pos(minX, minY, maxZ)
+                case B => Pos(maxX, minY, maxZ)
+                case C => Pos(maxX, maxX, maxZ)
+                case D => Pos(minX, maxX, maxZ)
+                case E => Pos(minX, minY, minZ)
+                case F => Pos(maxX, minY, minZ)
+                case G => Pos(maxX, maxX, minZ)
+                case H => Pos(minX, maxX, minZ)
+
+                case AB => midpoint(A, B)
+                case BC => midpoint(B, C)
+                case CD => midpoint(C, D)
+                case DA => midpoint(D, A)
+                case BF => midpoint(B, F)
+                case GC => midpoint(G, C)
+                case CB => midpoint(C, B)
+                case EF => midpoint(E, F)
+                case FG => midpoint(F, G)
+                case GH => midpoint(G, H)
+                case HE => midpoint(H, E)
+                case EA => midpoint(E, A)
+                case AD => midpoint(A, D)
+                case DH => midpoint(D, H)
+
+                case Front => midpoint(A, C)
+                case Right => midpoint(B, G)
+                case Back => midpoint(E, G)
+                case Left => midpoint(E, D)
+                case Top => midpoint(D, G)
+                case Bottom => midpoint(A, F)
+                
+                case Center => midpoint(Top, Bottom)
+            }
+        }
+
+        private def midpoint(a: AnchorType, b: AnchorType): Pos = {
+            resolve(a) + 0.5d * (resolve(b) - resolve(a))
+        }
+        
     }
 
 }
